4eeeb327d7451f12da45d65a8e57d5e749c8855d
newmediapilot <marcin@newmediapilot.com>
2020-09-09 00:48:21 -0400

    auto push

--- example-app/src/components/Counter/Counter.js
+++ example-app/src/components/Counter/Counter.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 1,
         imageURL: 'https://picsum.photos/200',
         bsClass: 'btn btn-danger'
     };
@@ -14,12 +14,14 @@ class Counter extends Component {
     };
 
     render() {
+        let classes = 'badge badge-primary m-2';
+        if (this.state.count > 0) classes = classes.replace('primary', 'warning');
         return (
             <div>
                 <div style={{color: 'red'}}>World</div>
                 <div style={this.styles}>Hello</div>
                 <img src={this.state.imageURL}/>
-                <div className="badge badge-primary m-2">{this.formatCount()}</div>
+                <div className={classes}>{this.formatCount()}</div>
                 <button className={this.state.bsClass}>Increment</button>
             </div>
         )
