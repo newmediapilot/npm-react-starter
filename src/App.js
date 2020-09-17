3ed4a75a622715ab74c9b51e03c33cc6ec312111
newmediapilot <marcin@newmediapilot.com>
2020-09-17 19:51:11 -0400

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/scss/bootstrap.scss';
-import login from "./components/login/login";
-import auth from "./components/auth/auth";
 
 function App() {
-    return (
-        auth()
-    )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
