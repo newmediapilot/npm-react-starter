25aafeb933277c27c670c9e5a4616cae346ec0d8
newmediapilot <marcin@newmediapilot.com>
2020-09-19 20:57:14 -0400

    add functional component template

---
+++ templates/function/component.js
@@ -0,0 +1,11 @@
+import styles from './TemplateName.module.scss';
+
+const TemplateName = () => {
+    return (
+        <div className={styles.TemplateName} data-testid="TemplateName">
+            TemplateName component works!
+        </div>
+    )
+};
+
+export default TemplateName;
